4e61606322327562aca4a307d59b08a32fbed358
Liubov Sobolevskaya <luba@Liubovs-MBP-2.attlocal.net>
2023-04-06 08:36:57 -0700

    add setInterval

--- assets/script.js
+++ assets/script.js
@@ -1,57 +1,67 @@
 $(function () {
   // Set the text of the  header to today's date using the dayjs library
-  $("#currentDay").text(dayjs().format("MMMM DD YYYY"));
-
-  // Get the current hour using dayjs 
-  currentHour = dayjs().hour();
-
-  // Get a reference to the root element of the page, which has the "container-lg" class
+  
   var rootEl = $('.container-lg'); 
   console.log(rootEl)
 
-  // Loop over the hours from 9am to 5pm
-  for (var i=9; i<18; i++){
-    // Create a new div element for each hour and add the appropriate classes based on the current time
-    var newHour = $('<div>');
-    newHour.attr('id', `hour-${i}`).addClass('row time-block');
-    if (i <currentHour ){
-      newHour.addClass('past');
-    }
-    else if (i === currentHour ){
-      newHour.addClass('present');
-    }
-    else{
-      newHour.addClass('future');
+  setInterval(function(){
+     
+    //reset large container innerHTML to ""
+    rootEl.empty()
+    $("#currentDay").text(dayjs().format("MMMM DD YYYY"));
+
+    // Get the current hour using dayjs 
+    currentHour = dayjs().hour();
+
+    // Get a reference to the root element of the page, which has the "container-lg" class
+
+
+    // Loop over the hours from 9am to 5pm
+    for (var i=9; i<18; i++){
+      // Create a new div element for each hour and add the appropriate classes based on the current time
+      var newHour = $('<div>');
+      newHour.attr('id', `hour-${i}`).addClass('row time-block');
+      if (i <currentHour ){
+        newHour.addClass('past');
+      }
+      else if (i === currentHour ){
+        newHour.addClass('present');
+      }
+      else{
+        newHour.addClass('future');
+      }
+
+      // Create a div element to display the hour and add the appropriate text based on the current time
+      var hourDiv = $('<div>').addClass('col-2 col-md-1 hour text-center py-3"');
+      if (i < 12){
+        hourDiv.text(`${i}AM`);
+      }
+      else if (i === 12){
+        hourDiv.text(`${i}PM`);
+      }
+      else if (i > 12){
+        hourDiv.text(`${i-12}PM`);
+      }
+    
+      // Create a textarea element for the user to enter their text, and set its current value to corresponding  text in teh local storage
+      var textArea = $('<textarea>').addClass('col-8 col-md-10 description').attr('rows', 3);
+      savedText = localStorage.getItem(`hour-${i}`);
+      if (savedText){
+        textArea.text(savedText);
+      }
+
+      // Create a button element to save the user's text and add a save icon to it
+      var btn =  $('<button>').addClass("btn saveBtn col-2 col-md-1").attr("aria-label", "save");
+      var saveIcon = $('<i>').addClass("fas fa-save").attr("aria-hidden", "true");
+      btn.append(saveIcon);
+
+      // Add the hour div, textarea, and button to the newHour div, and then add the newHour div to the larger container
+      newHour.append(hourDiv, textArea, btn);
+      rootEl.append(newHour);
     }
 
-    // Create a div element to display the hour and add the appropriate text based on the current time
-    var hourDiv = $('<div>').addClass('col-2 col-md-1 hour text-center py-3"');
-    if (i < 12){
-      hourDiv.text(`${i}AM`);
-    }
-    else if (i === 12){
-      hourDiv.text(`${i}PM`);
-    }
-    else if (i > 12){
-      hourDiv.text(`${i-12}PM`);
-    }
-  
-    // Create a textarea element for the user to enter their text, and set its current value to corresponding  text in teh local storage
-    var textArea = $('<textarea>').addClass('col-8 col-md-10 description').attr('rows', 3);
-    savedText = localStorage.getItem(`hour-${i}`);
-    if (savedText){
-      textArea.text(savedText);
-    }
-
-    // Create a button element to save the user's text and add a save icon to it
-    var btn =  $('<button>').addClass("btn saveBtn col-2 col-md-1").attr("aria-label", "save");
-    var saveIcon = $('<i>').addClass("fas fa-save").attr("aria-hidden", "true");
-    btn.append(saveIcon);
-
-    // Add the hour div, textarea, and button to the newHour div, and then add the newHour div to the larger container
-    newHour.append(hourDiv, textArea, btn);
-    rootEl.append(newHour);
-  }
+   
+  }, 1000);
 
   // Set up a click event handler on the root element that listens for clicks on save buttons
   rootEl.on('click', '.saveBtn', function (event) {
@@ -60,4 +70,5 @@ $(function () {
     var textToSave = $(`#${theHour}`).children().eq(1).val();
     localStorage.setItem(theHour, textToSave);
   })
+
 });
